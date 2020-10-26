9cd27787cb94c3526e38881540c8bf8c4f5437ff
Alex Fulbert <Alexfulb94@hotmail.fr>
2020-10-26 21:17:16 +0100

    etape 4

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Money money = new Money(42,"EUR");
+        System.out.println(money);
+    }
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,5 +1,19 @@
 package demo;
 
 public class Money {
+        int amount;
+        String currency ;
 
+        public Money(int amount, String currency){
+            this.amount = amount;
+            this.currency = currency;
+        }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
